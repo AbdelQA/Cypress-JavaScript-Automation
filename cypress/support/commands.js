e1d8e8fa2895103a614ae62494495c7d65f417c2
qakoala <96100804+qakoala@users.noreply.github.com>
2023-03-21 22:02:11 -0700

    Added 4 functionality tests and login command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,7 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('loginConduit', (email, password) => { 
+    cy.visit('https://demo.realworld.io/#/')
+    cy.get('.nav-item').contains('Sign in').click()
+    cy.get('input[placeholder="Email"]').type(email)
+    cy.get('input[placeholder="Password"]').type(password)
+    cy.get('button[type="submit"]').click()
+ })
